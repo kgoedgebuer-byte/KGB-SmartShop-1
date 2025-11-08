afe0e1c15cf0ca964f1d7affddcbf1696d586320
Kurt Goedgebuer <kurtgoedgebuer@iMac-van-Kurt.local>
2025-11-08 09:33:21 +0100

    Deploy 2025-11-08_08:33:21

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1995336111"
+    serviceWorkerVersion: "2237650444"
   }
 });

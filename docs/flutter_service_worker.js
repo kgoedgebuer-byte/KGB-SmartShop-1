de7668b4fff63125db14fb79e46a95debdf48eac
Kurt <kgoedgebuer-byte@users.noreply.github.com>
2025-11-11 13:11:56 +0100

    Auto deploy web build to GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "14daacb814d6a941a441e9a53621b0d6",
+const RESOURCES = {"flutter_bootstrap.js": "07d575d2e36be66223319acddb1f88f0",
 "version.json": "bb6450d1ac632c33bdc1009f6b45d72a",
 "index.html": "afeea8a7549f5cbe1bc1c9d9e4be5921",
 "/": "afeea8a7549f5cbe1bc1c9d9e4be5921",
